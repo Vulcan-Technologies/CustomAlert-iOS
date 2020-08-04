787e83d9457df1b563d54c064b1bc553282381a1
Mikael <mikaelm1013@gmail.com>
2020-08-03 19:20:16 -0700

    Finished part1 of the video

---
+++ customalert/ViewController.swift
@@ -0,0 +1,110 @@
+//
+//  ViewController.swift
+//  customalert
+//
+//  Created by Mikael Mukhsikaroyan on 8/1/20.
+//
+
+import UIKit
+
+extension UIView {
+    func activate(_ constraints: [NSLayoutConstraint]) {
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate(constraints)
+    }
+}
+
+class ViewController: UIViewController {
+
+    lazy var button1: UIButton = {
+        let view = UIButton(type: .system)
+        view.setTitle("Show iOS Alert", for: .normal)
+        view.backgroundColor = .systemBlue
+        view.setTitleColor(.white, for: .normal)
+        view.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        view.addTarget(self, action: #selector(didTapButton1(_:)), for: .touchUpInside)
+
+        return view
+    }()
+
+    lazy var button2: UIButton = {
+        let view = UIButton(type: .system)
+        view.setTitle("Show Vulcan Alert", for: .normal)
+        view.backgroundColor = .systemBlue
+        view.setTitleColor(.white, for: .normal)
+        view.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        view.addTarget(self, action: #selector(didTapButton2(_:)), for: .touchUpInside)
+
+        return view
+    }()
+
+    let buttonsStackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .vertical
+        view.distribution = .equalSpacing
+        view.spacing = 6
+
+        return view
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemRed
+
+        view.addSubview(buttonsStackView)
+        buttonsStackView.activate([
+            view.bottomAnchor.constraint(equalTo: buttonsStackView.bottomAnchor, constant: 50),
+            buttonsStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            buttonsStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+
+        buttonsStackView.addArrangedSubview(button1)
+        buttonsStackView.addArrangedSubview(button2)
+    }
+
+    @objc func didTapButton1(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Title", message: "Message", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { _ in
+            print("Cancel tapped")
+        }))
+
+        alert.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { _ in
+            print("Delete tapped")
+        }))
+
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { _ in
+            print("Ok tapped")
+        }))
+
+        alert.addTextField { field in
+            field.backgroundColor = .systemPink
+        }
+
+        present(alert, animated: true, completion: nil)
+    }
+
+    @objc func didTapButton2(_ sender: UIButton) {
+        let alert = VulcanAlert(title: "Title", message: "This is the message")
+
+        alert.addAction(VulcanAction(title: "Cancel", style: .cancel, completion: {
+            print("Cancel tapped")
+        }))
+
+        alert.addAction(VulcanAction(title: "Ok", style: .default, completion: {
+            print("Ok tapped")
+        }))
+
+        alert.addAction(VulcanAction(title: "Delete", style: .destructive, completion: {
+            print("Delete tapped")
+        }))
+
+        alert.addTextField { field in
+            field.placeholder = "Enter username"
+        }
+
+        present(alert, animated: false, completion: nil)
+    }
+
+}
+
---
+++ customalert/VulcanAlert.swift
@@ -0,0 +1,245 @@
+//
+//  VulcanAlert.swift
+//  customalert
+//
+//  Created by Mikael Mukhsikaroyan on 8/2/20.
+//
+
+import UIKit
+
+class VulcanButton: UIButton {
+    var completion: (()->Void)?
+}
+
+class AlertRootView: UIView {
+
+    let blurView: UIVisualEffectView = {
+        let blurEffect = UIBlurEffect(style: .regular)
+        let view = UIVisualEffectView(effect: blurEffect)
+
+        return view
+    }()
+
+    private let container: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 8
+
+        return view
+    }()
+
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        label.textColor = .black
+        label.textAlignment = .center
+
+        return label
+    }()
+
+    private let messageLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.textColor = .black
+        label.textAlignment = .center
+
+        return label
+    }()
+
+    private let buttonsStackView: UIStackView = {
+        let view = UIStackView()
+        view.spacing = 4
+        view.axis = .vertical
+        view.distribution = .fill
+
+        return view
+    }()
+
+    private let textFieldsStackView: UIStackView = {
+        let view = UIStackView()
+        view.spacing = 4
+        view.axis = .vertical
+        view.distribution = .fill
+
+        return view
+    }()
+
+    // MARK: Init
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        configureView()
+        configureConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: View Configuration
+
+    func configureView() {
+        backgroundColor = .clear
+
+        addSubview(blurView)
+        addSubview(container)
+        container.addSubview(titleLabel)
+        container.addSubview(messageLabel)
+        container.addSubview(buttonsStackView)
+        container.addSubview(textFieldsStackView)
+    }
+
+    func configureConstraints() {
+        blurView.activate([
+            blurView.topAnchor.constraint(equalTo: topAnchor),
+            blurView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            blurView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            blurView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+
+        let containerPadding: CGFloat = 20
+        container.activate([
+            container.leadingAnchor.constraint(equalTo: leadingAnchor, constant: containerPadding),
+            trailingAnchor.constraint(equalTo: container.trailingAnchor, constant: containerPadding),
+            container.centerYAnchor.constraint(equalTo: centerYAnchor),
+            container.heightAnchor.constraint(greaterThanOrEqualToConstant: 10)
+        ])
+
+        let labelPadding: CGFloat = 8
+        titleLabel.activate([
+            titleLabel.topAnchor.constraint(equalTo: container.topAnchor, constant: labelPadding),
+            titleLabel.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: labelPadding),
+            container.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor, constant: labelPadding)
+        ])
+
+        messageLabel.activate([
+            messageLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor),
+            messageLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            messageLabel.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor)
+        ])
+
+        let stackViewPadding: CGFloat = 8
+        textFieldsStackView.activate([
+            textFieldsStackView.topAnchor.constraint(equalTo: messageLabel.bottomAnchor, constant: stackViewPadding),
+            textFieldsStackView.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: stackViewPadding),
+            container.trailingAnchor.constraint(equalTo: textFieldsStackView.trailingAnchor, constant: stackViewPadding)
+        ])
+
+        buttonsStackView.activate([
+            container.bottomAnchor.constraint(equalTo: buttonsStackView.bottomAnchor, constant: stackViewPadding),
+            buttonsStackView.leadingAnchor.constraint(equalTo: textFieldsStackView.leadingAnchor),
+            buttonsStackView.trailingAnchor.constraint(equalTo: textFieldsStackView.trailingAnchor),
+            buttonsStackView.topAnchor.constraint(equalTo: textFieldsStackView.bottomAnchor, constant: stackViewPadding)
+        ])
+    }
+
+    // MARK: Helpers
+
+    func addTextField() -> UITextField {
+        let field = UITextField()
+        field.layer.cornerRadius = 4
+        field.backgroundColor = .systemGray5
+        field.font = UIFont.preferredFont(forTextStyle: .body)
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 8, height: 0))
+        field.leftViewMode = .always
+        field.rightView = UIView(frame: CGRect(x: 0, y: 0, width: 8, height: 0))
+        field.rightViewMode = .always
+
+        field.activate([
+            field.heightAnchor.constraint(equalToConstant: 30)
+        ])
+
+        textFieldsStackView.addArrangedSubview(field)
+
+        return field
+    }
+
+    func addButton(_ action: VulcanAction) -> VulcanButton {
+        let button = VulcanButton(type: .system)
+        button.layer.cornerRadius = 4
+        button.setTitle(action.title, for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
+
+        switch action.style {
+        case .cancel:
+            button.backgroundColor = .systemGray4
+            button.setTitleColor(.systemBlue, for: .normal)
+        case .default:
+            button.backgroundColor = .systemBlue
+        case .destructive:
+            button.backgroundColor = .systemRed
+        }
+
+        buttonsStackView.addArrangedSubview(button)
+
+        return button
+    }
+}
+
+class VulcanAlert: UIViewController {
+
+    private let rootView = AlertRootView()
+    private(set) var textFields = [UITextField]()
+    private(set) var actions = [VulcanAction]()
+    private let alertTitle: String?
+    private let message: String?
+
+    // MARK: Lifecycle
+
+    override func loadView() {
+        view = rootView
+    }
+
+    init(title: String?, message: String?) {
+        self.alertTitle = title
+        self.message = message
+        super.init(nibName: nil, bundle: nil)
+        modalPresentationStyle = .overFullScreen
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: Actions
+
+    @objc private func didTapButton(_ sender: VulcanButton) {
+        sender.completion?()
+        dismiss(animated: false, completion: nil)
+    }
+
+    func addAction(_ action: VulcanAction) {
+        self.actions.append(action)
+
+        let button = rootView.addButton(action)
+        button.completion = action.completion
+        button.addTarget(self, action: #selector(didTapButton(_:)), for: .touchUpInside)
+    }
+
+    func addTextField(_ completion: (_ field: UITextField)->Void) {
+        let field = rootView.addTextField()
+        self.textFields.append(field)
+
+        completion(field)
+    }
+}
+
+class VulcanAction {
+    enum Style {
+        case `default`
+        case cancel
+        case destructive
+    }
+
+    let style: Style
+    let title: String
+    let completion: (()->Void)?
+
+    init(title: String, style: Style, completion: (()->Void)?) {
+        self.title = title
+        self.style = style
+        self.completion = completion
+    }
+}
